1918da3e3a8effb16bbd2abe2cc5337f4272ff4c
User 1 <gowridog@gmail.com>
2025-03-27 16:24:14 -0400

    Updated new files

---
+++ src/Test/SearchPage.java
@@ -0,0 +1,12 @@
+package Test;
+
+public class SearchPage {
+
+	public static void main(String[] args) {
+		System.out.println("LP T");
+		String employer = "Fitch";
+		
+		System.out.println(employer);
+	}
+
+}
--- src/main/HomePage.java
+++ src/main/HomePage.java
@@ -4,6 +4,8 @@ public class HomePage {
 
 	public static void main(String[] args) {
 		System.out.println("HP T");
+		String user = "Admin";
+		System.out.println(user);
 
 	}
 
